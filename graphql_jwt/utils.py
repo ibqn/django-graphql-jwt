3d09322b5abec63dcb1fab174b2e0609078b4da9
ibqn <evgen.ibqn@gmail.com>
2021-03-16 23:11:08 +0100

    Token cannot be created ('str' has no attribute 'decode') #243

--- graphql_jwt/utils.py
+++ graphql_jwt/utils.py
@@ -14,32 +14,37 @@ from .settings import jwt_settings
 def jwt_payload(user, context=None):
     username = user.get_username()
 
-    if hasattr(username, 'pk'):
+    if hasattr(username, "pk"):
         username = username.pk
 
     payload = {
         user.USERNAME_FIELD: username,
-        'exp': datetime.utcnow() + jwt_settings.JWT_EXPIRATION_DELTA,
+        "exp": datetime.utcnow() + jwt_settings.JWT_EXPIRATION_DELTA,
     }
 
     if jwt_settings.JWT_ALLOW_REFRESH:
-        payload['origIat'] = timegm(datetime.utcnow().utctimetuple())
+        payload["origIat"] = timegm(datetime.utcnow().utctimetuple())
 
     if jwt_settings.JWT_AUDIENCE is not None:
-        payload['aud'] = jwt_settings.JWT_AUDIENCE
+        payload["aud"] = jwt_settings.JWT_AUDIENCE
 
     if jwt_settings.JWT_ISSUER is not None:
-        payload['iss'] = jwt_settings.JWT_ISSUER
+        payload["iss"] = jwt_settings.JWT_ISSUER
 
     return payload
 
 
 def jwt_encode(payload, context=None):
-    return jwt.encode(
+    token = jwt.encode(
         payload,
         jwt_settings.JWT_PRIVATE_KEY or jwt_settings.JWT_SECRET_KEY,
         jwt_settings.JWT_ALGORITHM,
-    ).decode('utf-8')
+    )
+
+    if isinstance(token, bytes):
+        return token.decode("utf-8")
+
+    return token
 
 
 def jwt_decode(token, context=None):
@@ -48,7 +53,7 @@ def jwt_decode(token, context=None):
         jwt_settings.JWT_PUBLIC_KEY or jwt_settings.JWT_SECRET_KEY,
         jwt_settings.JWT_VERIFY,
         options={
-            'verify_exp': jwt_settings.JWT_VERIFY_EXPIRATION,
+            "verify_exp": jwt_settings.JWT_VERIFY_EXPIRATION,
         },
         leeway=jwt_settings.JWT_LEEWAY,
         audience=jwt_settings.JWT_AUDIENCE,
@@ -58,7 +63,7 @@ def jwt_decode(token, context=None):
 
 
 def get_http_authorization(request):
-    auth = request.META.get(jwt_settings.JWT_AUTH_HEADER_NAME, '').split()
+    auth = request.META.get(jwt_settings.JWT_AUTH_HEADER_NAME, "").split()
     prefix = jwt_settings.JWT_AUTH_HEADER_PREFIX
 
     if len(auth) != 2 or auth[0].lower() != prefix.lower():
@@ -68,7 +73,7 @@ def get_http_authorization(request):
 
 def get_token_argument(request, **kwargs):
     if jwt_settings.JWT_ALLOW_ARGUMENT:
-        input_fields = kwargs.get('input')
+        input_fields = kwargs.get("input")
 
         if isinstance(input_fields, dict):
             kwargs = input_fields
@@ -78,8 +83,7 @@ def get_token_argument(request, **kwargs):
 
 
 def get_credentials(request, **kwargs):
-    return (get_token_argument(request, **kwargs) or
-            get_http_authorization(request))
+    return get_token_argument(request, **kwargs) or get_http_authorization(request)
 
 
 def get_payload(token, context=None):
@@ -88,9 +92,9 @@ def get_payload(token, context=None):
     except jwt.ExpiredSignature:
         raise exceptions.JSONWebTokenExpired()
     except jwt.DecodeError:
-        raise exceptions.JSONWebTokenError(_('Error decoding signature'))
+        raise exceptions.JSONWebTokenError(_("Error decoding signature"))
     except jwt.InvalidTokenError:
-        raise exceptions.JSONWebTokenError(_('Invalid token'))
+        raise exceptions.JSONWebTokenError(_("Invalid token"))
     return payload
 
 
@@ -106,12 +110,12 @@ def get_user_by_payload(payload):
     username = jwt_settings.JWT_PAYLOAD_GET_USERNAME_HANDLER(payload)
 
     if not username:
-        raise exceptions.JSONWebTokenError(_('Invalid payload'))
+        raise exceptions.JSONWebTokenError(_("Invalid payload"))
 
     user = jwt_settings.JWT_GET_USER_BY_NATURAL_KEY_HANDLER(username)
 
-    if user is not None and not getattr(user, 'is_active', True):
-        raise exceptions.JSONWebTokenError(_('User is disabled'))
+    if user is not None and not getattr(user, "is_active", True):
+        raise exceptions.JSONWebTokenError(_("User is disabled"))
     return user
 
 
@@ -122,14 +126,14 @@ def refresh_has_expired(orig_iat, context=None):
 
 def set_cookie(response, key, value, expires):
     kwargs = {
-        'expires': expires,
-        'httponly': True,
-        'secure': jwt_settings.JWT_COOKIE_SECURE,
-        'path': jwt_settings.JWT_COOKIE_PATH,
-        'domain': jwt_settings.JWT_COOKIE_DOMAIN,
+        "expires": expires,
+        "httponly": True,
+        "secure": jwt_settings.JWT_COOKIE_SECURE,
+        "path": jwt_settings.JWT_COOKIE_PATH,
+        "domain": jwt_settings.JWT_COOKIE_DOMAIN,
     }
     if django.VERSION >= (2, 1):
-        kwargs['samesite'] = jwt_settings.JWT_COOKIE_SAMESITE
+        kwargs["samesite"] = jwt_settings.JWT_COOKIE_SAMESITE
 
     response.set_cookie(key, value, **kwargs)
 
